33f96cdbee4d40623da0729af9267703a3ac379f
MaksItLab <maksim.ardintsev@mail.ru>
2024-03-28 23:05:24 +0300

    Добавьте файлы проекта.

---
+++ lr4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr4
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int M = 26;
+			int K = 4;
+
+			string str = Console.ReadLine();
+			string strClose = "";
+			string strOpen = "";
+
+			char[] arrOpentext = str.ToCharArray();
+            Console.WriteLine();
+			char[] arrCloseText = new char[arrOpentext.Length];
+			for (int i = 0; i < arrOpentext.Length; i++)
+			{
+				arrCloseText[i] = (char)(((arrOpentext[i] + K) % M) + 97);
+			}
+
+			strClose = String.Concat<char>(arrCloseText);
+			Console.WriteLine(strClose);
+
+		}
+	}
+}
